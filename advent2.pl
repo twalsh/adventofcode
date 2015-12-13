d52f7ead27845e4feace63f48ab31b25c927b3cd
Tom Walsh <walshtp@gmail.com>
2015-12-13 19:30:47 +0000

    Working advent2.pl

--- advent2.pl
+++ advent2.pl
@@ -1,31 +1,17 @@
-#%#lang racket
-#
-#(define (get-ribbon dimensions)
-#  (+ (* 2 (apply + (take (sort dimensions <) 2)))
-#     (apply * dimensions)))
-#
-sub dim_min (@dimensions){
-	return @dimensions.sort[0..1];
+
+
+sub min (@dimensions) {
+	return @dimensions.sort({$^a <=> $^b })[0..1];
 }
 
 sub get_ribbon (@dimensions) {
-	return 2 * [+] @dimensions.sort[0..1] + [*] @dimensions;
+	return 2 * ([+] min(@dimensions)) + [*] @dimensions;
 }
 
 sub get_paper (@dimensions){
 	my ($l,$w,$h) = @dimensions;
-	return 2 * ( $l*$w + $w*$h + $h*$l) + [*] @dimensions.sort[0..1];
+	return 2 * ( $l*$w + $w*$h + $h*$l) + [*] min(@dimensions);
 }
-#(define (get-paper dimensions)
-#  (match-let (((list l w h) dimensions))
-#    (+ (* 2
-#          (+
-#           (* l w)
-#           (* w h)
-#           (* h l)))
-#       ; Extra paper
-#       (apply * (take (sort dimensions <) 2)))))
-#
 my $total_paper = 0;
 my $total_ribbon = 0;
 
@@ -34,16 +20,6 @@ for 'input2.txt'.IO.lines -> $line {
 	$total_paper += get_paper(@dimensions);
 	$total_ribbon += get_ribbon(@dimensions);
 }
-#(define (read-input in)
-#  (let loop ((line (read-line in)) (total-paper 0) (total-ribbon 0))
-#    (if (eof-object? line)
-#        (cons total-paper total-ribbon)
-#        (let ((dimensions (map string->number (string-split line "x"))))
-#          (let ((paper (get-paper dimensions))
-#                (ribbon (get-ribbon dimensions)))
-#            (loop (read-line in) (+ total-paper paper) (+ total-ribbon ribbon))
-#            )))))
-#
-#
-#(call-with-input-file "input2.txt" read-input)
-#
+
+say $total_paper;
+say $total_ribbon;
