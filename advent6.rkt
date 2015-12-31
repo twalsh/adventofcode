0a07278da48882b8b62d105816ec5d9e3fab9b85
Tom Walsh <walshtp@gmail.com>
2015-12-31 18:14:55 +0000

    Working version using match-let to parse command lines

--- advent6.rkt
+++ advent6.rkt
@@ -3,39 +3,22 @@
 (require srfi/25)
 (require "advent-utils.rkt")
 
-(define (turn-on lights x y)
-              (array-set! lights x y #t))
-(define (turn-off lights x y)
-              (array-set! lights x y #f))
-(define (toggle lights x y)
-              (array-set! lights x y (not (array-ref lights x y))))
+(define op-table (hash
+                  "turn on"  (lambda (lights x y) (array-set! lights x y #t))
+                  "turn off" (lambda (lights x y) (array-set! lights x y #f))
+                  "toggle"   (lambda (lights x y) (array-set! lights x y (not (array-ref lights x y))))))
                                                      
 (define lights (make-array (shape 0 1000 0 1000) #f))
 
 (define lines (call-with-input-file "input6.txt" read-lines))
 
 (define (process-line line)
-  (define (coord fields)
-    (map string->number (flatten
-     (map (lambda (s) (string-split s ","))
-     (list (car fields) (caddr fields))
-     )
-    )))
-  (display line)(newline)
   (match-let
       (((pregexp "^(toggle|turn (on|off)) (\\d+),(\\d+) through (\\d+),(\\d+)"
-                (list _ instr on/off x0 y0 x1 y1)) line))
-    instr))
-   ; (let ((coords (map string->number (list x0 y0 x1 y1)))
-    
-;  #;(let ((fields (string-split line)))
- ;  (if (string=? (car fields) "toggle")
-  ;;     (list toggle (coord (cdr fields)))
-    ;;   (if (string=? (cadr fields) "on")
-      ;;     (list turn-on  (coord (cddr fields)))
-        ;   (list turn-off (coord (cddr fields)))))))
+                (list _ op _ x0 y0 x1 y1)) line))
+    (list (hash-ref op-table op) (map string->number (list x0 y0 x1 y1)))
+    ))
 
-               
 (for ((order (map process-line lines)))
   (match-let (((list proc (list x0 y0 x1 y1)) order))
     (for* ((x (in-range x0 (add1 x1)))
