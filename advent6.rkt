25f2b08402f18e1f206b6e43d76addae2f07f52c
Tom Walsh <walshtp@gmail.com>
2015-12-31 18:46:31 +0000

    Remove op-table

--- advent6.rkt
+++ advent6.rkt
@@ -2,12 +2,7 @@
 
 (require srfi/25)
 (require "advent-utils.rkt")
-
-(define op-table (hash
-                  "turn on"  (lambda (lights x y) (array-set! lights x y #t))
-                  "turn off" (lambda (lights x y) (array-set! lights x y #f))
-                  "toggle"   (lambda (lights x y) (array-set! lights x y (not (array-ref lights x y))))))
-                                                     
+                                        
 (define lights (make-array (shape 0 1000 0 1000) #f))
 
 (define lines (call-with-input-file "input6.txt" read-lines))
@@ -15,17 +10,20 @@
 (define (process-line line)
   (match-let
       (((pregexp "^(toggle|turn (on|off)) (\\d+),(\\d+) through (\\d+),(\\d+)"
-                (list _ op _ x0 y0 x1 y1)) line))
-    (list (hash-ref op-table op) (map string->number (list x0 y0 x1 y1)))
+                (list _ op arg x0 y0 x1 y1)) line))
+    (list arg (map string->number (list x0 y0 x1 y1)))
     ))
 
 (for ((order (map process-line lines)))
-  (match-let (((list proc (list x0 y0 x1 y1)) order))
+  (match-let (((list arg (list x0 y0 x1 y1)) order))
     (for* ((x (in-range x0 (add1 x1)))
            (y (in-range y0 (add1 y1))))
-      (proc lights x y))))
+      (let ((val (match arg
+                   (#f (not (array-ref lights x y)))
+                   ("on" #t)
+                   ("off" #f))))
+        (array-set! lights x y val)))))
 
 (for*/sum ((x (in-range 0 1000))
            (y (in-range 0 1000)))
            (if (array-ref lights x y) 1 0))
-           
