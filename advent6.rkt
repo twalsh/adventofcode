1e2c8a7c918d338024865009f9b97a3e7f4d32ad
Tom Walsh <walshtp@gmail.com>
2015-12-31 12:53:10 +0000

    6a complete

--- advent6.rkt
+++ advent6.rkt
@@ -1,23 +1,40 @@
 #lang racket
 
+(require srfi/25)
 (require "advent-utils.rkt")
 
-(define instr
-  (hash "on" (lambda (lights xy)
-              (hash-update lights xy (lambda (x) #t) #t))
-        "off" (lambda (lights xy)
-                (hash-update lights xy (lambda (x) #f) #f))
-        "toggle" (lambda (lights xy)
-                   (hash-update lights xy not 0))))
-
-(define lights (make-immutable-hash))
+(define (turn-on lights x y)
+              (array-set! lights x y #t))
+(define (turn-off lights x y)
+              (array-set! lights x y #f))
+(define (toggle lights x y)
+              (array-set! lights x y (not (array-ref lights x y))))
+                                                     
+(define lights (make-array (shape 0 1000 0 1000) #f))
 
 (define lines (call-with-input-file "input6.txt" read-lines))
 
 (define (process-line line)
+  (define (coord fields)
+    (map string->number (flatten
+     (map (lambda (s) (string-split s ","))
+     (list (car fields) (caddr fields))
+     )
+    )))
   (let ((fields (string-split line)))
-    (let ((instr (if (string=? (car fields) "toggle") (cons "toggle" 
-                     (cadr fields))))
-      instr)))
+   (if (string=? (car fields) "toggle")
+       (list toggle (coord (cdr fields)))
+       (if (string=? (cadr fields) "on")
+           (list turn-on  (coord (cddr fields)))
+           (list turn-off (coord (cddr fields)))))))
+               
+(for ((order (map process-line lines)))
+  (match-let (((list proc (list x0 y0 x1 y1)) order))
+    (for* ((x (in-range x0 (add1 x1)))
+           (y (in-range y0 (add1 y1))))
+      (proc lights x y))))
 
-(map process-line lines)
+(for*/sum ((x (in-range 0 1000))
+           (y (in-range 0 1000)))
+           (if (array-ref lights x y) 1 0))
+           
