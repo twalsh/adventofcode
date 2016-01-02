a6a5d8cc9866e10c5a614829409876402133e5f6
Tom Walsh <walshtp@gmail.com>
2016-01-02 15:35:17 +0000

    Replace hash with structs in rulesets

--- advent5.rkt
+++ advent5.rkt
@@ -2,69 +2,73 @@
 (require rackunit)
 (require "advent-utils.rkt")
 
+(struct rule (desc fn))
+(struct ruleset (nice naughty rules expected-rules-output))
+
 (define rulesets
   (list
-   (hash
-    'nice (list "ugknbfddgicrmopn" "aaa")
-    'naughty (vector "jchzalrnumimnmhp" "haegwjzuvuyypxyu" "dvszwmarrgswjxmb")
-    'rules 
+   (ruleset
+    '("ugknbfddgicrmopn" "aaa")
+    #("jchzalrnumimnmhp" "haegwjzuvuyypxyu" "dvszwmarrgswjxmb")
     (list
-     (cons "At least 3 vowels"
+     (rule "At least 3 vowels"
            (lambda (s) (>= (length (regexp-match* #px"[aeiou]" s)) 3)))
-     (cons "At least 1 doubled letter" 
+     (rule "At least 1 doubled letter" 
            (lambda (s) (not (empty? (regexp-match* #px"([a-z])\\1" s)))))
-     (cons "No ab,cd,pq or xy" 
+     (rule "No ab,cd,pq or xy" 
            (lambda ( s) (not (regexp-match #px"(ab|cd|pq|xy)" s))))
-     )
-    'expected-rules-output 
-    (vector (vector #t #t #f) (vector #f #t #t) (vector #t #f #t))
+     ) 
+    #(#(#t #t #f) #(#f #t #t) #(#t #f #t))
     )
-   (hash
-    'nice (list "qjhvhtzxzqqjkmpb" "xxyxx")
-    'naughty (vector "uurcxstgmygtbstg" "ieodomkazucvgmuy")
-    'rules 
+   (ruleset
+    '("qjhvhtzxzqqjkmpb" "xxyxx")
+    #("uurcxstgmygtbstg" "ieodomkazucvgmuy")
     (list
-     (cons "Pair at least twice"
+     (rule "Pair at least twice"
            (lambda (s)
              (not (empty? (regexp-match* #px"([a-z]{2}).*\\1" s)))))
-     (cons "X*X"
+     (rule "X*X"
            (lambda (s)
              (>= (length (regexp-match* #px"([a-z])\\w\\1" s)) 1)))
      )
-    'expected-rules-output 
-    (vector (vector #t #f) (vector #f #t))
+    #(#(#t #f) #(#f #t))
     )
    ))
 
-(define (rule-filter ruleset)
-  (let ((rules (hash-ref ruleset 'rules)))
-    (lambda (s)
-      (andmap (lambda (rule) ((cdr rule) s)) rules))))
+(define (rule-filter rs)
+  (lambda (s)
+    (andmap (lambda (r) ((rule-fn r) s)) (ruleset-rules rs))))
 
-(define (check-ruleset ruleset)
-  (let ((rules (hash-ref ruleset 'rules))
-        (naughty-s (hash-ref ruleset 'naughty))
-        (nice-s (hash-ref ruleset 'nice))
-        (expected-rules-output (hash-ref ruleset 'expected-rules-output)))
+(define (check-ruleset rs)
+  (let ((rules     (ruleset-rules rs))
+        (naughty-s (ruleset-naughty rs))
+        (nice-s    (ruleset-nice rs))
+        (expected-rules-output (ruleset-expected-rules-output rs)))
     (for ((i (range (length rules))))
-      (let ((rule (list-ref rules i))
+      (let ((r (list-ref rules i))
             (expected (vector-ref expected-rules-output i)))
         (for ((j (range (vector-length naughty-s))))
           (let ((s (vector-ref naughty-s j)))
             (check-equal? 
-             ((cdr rule) s) 
+             ((rule-fn r) s) 
              (vector-ref expected j) 
-             (format "~s ~s" s (car rule))
+             (format "~s ~s" s (rule-desc r))
              )
             ))))
     
     (for ((s nice-s))
-      (check-equal? ((rule-filter ruleset) s) #t s))
+      (check-equal? ((rule-filter rs) s) #t s))
     ))
 
 (define lines (call-with-input-file "input5.txt" read-lines))
 (check-ruleset (first rulesets))
-(count (rule-filter (first rulesets)) lines) 
+(define part-one (count (rule-filter (first rulesets)) lines)) 
 
 (check-ruleset (second rulesets))
-(count (rule-filter (second rulesets)) lines) 
+(define part-two (count (rule-filter (second rulesets)) lines))
+
+(check-equal? part-one 238)
+(check-equal? part-two 69)
+
+(printf "part-one: ~s~n" part-one)
+(printf "part-two: ~s~n" part-two)
