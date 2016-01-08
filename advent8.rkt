4ab1404cf0ad9d73ca1613826fc1ecd86bc794a0
Tom Walsh <walshtp@gmail.com>
2016-01-08 17:34:17 +0000

    Part One solved

--- advent8.rkt
+++ advent8.rkt
@@ -12,24 +12,22 @@
 
 (define lines (call-with-input-file "input8.txt" read-lines))
 
-(define test-strings (list "\"\"" "\"abc\"" "\"aaa\"aaa\"" "\"\x27\""))
-
 (define (strlen bs)
-  (let loop ((s (rest (bytes->list bs))) (l 0))
+  (let loop ((s (bytes->list bs)) (l 0))
     (if (empty? s)
-        (- l 1)
+        (- l 2)
         (match s
-          ((list 92 34 ...) (loop (drop s 2) (+ l 1)))
+          ((list 92 34 _ ...) (loop (drop s 2) (+ l 1)))
           ((list 92 120 _ _ ...) (loop (drop s 4) (+ l 1)))
-          ((list 92 92 ...) (loop (drop s 2) (+ l 1)))
+          ((list 92 92 _ ...) (loop (drop s 2) (+ l 1)))
           ((list 34 ..1) (loop (rest s) (+ l 1)))
           (_ (loop (rest s) (+ l 1)))))))
-
-(for ((line lines))
-  (printf "~s ~s ~s~n" line (strlen line) (bytes-length line)))
 
 (define strlens (apply + (map strlen lines)))
 (define bytelens (apply + (map bytes-length lines)))
 
-(- strlens bytelens)
+(define part-one (- bytelens strlens))
+
+(printf "Day 8. Part One: ~s~n" part-one)
+(check-equal? part-one 1342)
 
