a47be09a337d21c52de46a4b305d04f8a2d176f4
Tom Walsh <walshtp@gmail.com>
2016-01-09 00:04:32 +0000

    Day 10 solved

--- advent10.rkt
+++ advent10.rkt
@@ -6,15 +6,30 @@
          (string->list (number->string n)))
        '(1 11 21 1211 111221)))
 
-(define (expand s)
-  (let loop ((c (first s)) (r (rest s)) (n 0))
-    (if (empty? r)
-        (cons (add1 n) c)
-        (if (char=? c (first r))
-            (loop (first r) (rest r) (+ n 1))
-            (cons (cons n c) (loop (first r) (rest r) 1)))))) 
-    
-(map expand test-sequences)
+(define (expand seq)
+  (flatten (map (lambda (p)
+                  (cons (integer->char (+ 48 (car p))) (cdr p)))
+                (rest (reverse (let loop ((r seq) (c #\_) (n 0) (p '()))
+                                 (if (empty? r)
+                                     (cons (cons n c) p)
+                                     (if (char=? c (first r))
+                                         (loop (rest r) c (+ n 1) p)
+                                         (loop (rest r) (first r) 1 (cons (cons n c) p))))))))))
 
-(for ((s test-sequences))
-  (printf "~s -> ~s~n" s (expand s)))
+(define input (string->list "1113222113"))
+
+(define (expand-iter e n)
+  (for/fold
+   ((e input))
+   ((i (in-range n)))
+    (values (expand e))))
+
+(define expanded-input (expand-iter input 40))
+
+(define part-one (length expanded-input))
+(printf "Day 10. Part One: ~s~n" part-one)
+(check-equal? part-one 252594)
+
+(define part-two (length (expand-iter input 50)))
+(printf "Day 10. Part Two: ~s~n" part-two)
+(check-equal? part-one 252594)
