9935bd7259a3a6af5ebe5bc241a7287076a72048
Tom Walsh <walshtp@gmail.com>
2016-01-12 22:30:30 +0000

    Added flight-path

--- advent14.rkt
+++ advent14.rkt
@@ -2,20 +2,50 @@
 
 (require rackunit)
 
-(define (distance speed range rest-time travel-time)
-  (let loop ((t 0) (flight-time 0) (state 'flying) (state-time 0))
-    (if (= t travel-time)
-        (* flight-time speed)
-        (if (eq? state 'flying)
-            (if (= state-time range)
-                (loop (add1 t) flight-time 'resting 0)
-                (loop (add1 t) (add1 flight-time) 'flying (add1 state-time)))
-            (if (= state-time rest-time)
-                (loop (add1 t) flight-time 'flying 0)
-                (loop (add1 t) flight-time 'resting (add1 state-time)))))))
+(struct deer (speed range rest-time))
 
-(define part-one (max (distance 14 10 127 2503)
-                      (distance 16 11 162 2503)))
+(define (flight-path travel-time)
+  (lambda (d)
+    (let ((range (deer-range d)) (rest-time (deer-rest-time d)))
+      (let loop ((fp '())
+                 (t 0)
+                 (ft 0) (state 'flying) (state-time 0))
+        (if (= t travel-time)
+            fp
+            (if (eq? state 'flying)
+                (if (= state-time range)
+                    (cons ft (loop fp (add1 t) ft 'resting 0))
+                    (cons (add1 ft) (loop fp (add1 t) (add1 ft) 'flying (add1 state-time))))
+                (if (= state-time rest-time)
+                    (cons ft (loop fp (add1 t) ft 'flying 0))
+                    (cons ft (loop fp (add1 t) ft 'resting (add1 state-time))))))))))
+
+(define (distance travel-time)
+  (lambda (d)
+    (* (deer-speed d)
+       (last ((flight-path travel-time) d)))))
+
+(define dl (list (deer 14 10 127)
+                 (deer 16 11 162)))
+
+((distance 1) (car dl))
+((distance 10) (car dl))
+((distance 1000) (car dl))
+
+(define part-one (apply max (map (distance 2503) dl)))
 
 (printf "Day 14. Part One: ~s~n" part-one)
 (check-equal? part-one 2640)
+
+(define paths (map (flight-path 140) dl))
+
+(map length paths)
+
+(for/fold ((score1 0) (score2 0))
+  ((f1 (car paths)) (f2 (cadr paths)))
+  (cond ((> f1 f2)
+         (values (add1 score1) score2))
+        ((< f1 f2)
+         (values score1 (add1 score2)))
+        (else
+         (values (add1 score1) (add1 score2)))))
