6c1ce34d260d475af0ee9e38f0f3aaeb07849a38
Tom Walsh <walshtp@gmail.com>
2016-01-12 08:32:33 +0000

    devel

--- advent12.rkt
+++ advent12.rkt
@@ -1,1 +1,13 @@
 #lang racket
+
+(require json)
+(require rackunit)
+
+(define input (with-input-from-file "input12.txt" (lambda () (read-json))))
+
+(define (process-list in)
+  (cond ((list? in) (for-each process-list in))
+        ((hash? in) (for-each process-list (hash-values in)))
+        (else in)))
+
+(process-list input)
