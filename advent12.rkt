efbbfffae85fb2e3bf9c46aa58951acb7c6279b2
Tom Walsh <walshtp@gmail.com>
2016-01-12 08:42:56 +0000

    Part One solved

--- advent12.rkt
+++ advent12.rkt
@@ -5,9 +5,15 @@
 
 (define input (with-input-from-file "input12.txt" (lambda () (read-json))))
 
-(define (process-list in)
-  (cond ((list? in) (for-each process-list in))
-        ((hash? in) (for-each process-list (hash-values in)))
-        (else in)))
+(define (sum-js in)
+  (apply + (flatten (cond ((list? in) (map sum-js in))
+        ((hash? in) (map sum-js (hash-values in)))
+        (else
+         (if (number? in)
+             in
+             '()))))))
 
-(process-list input)
+(define part-one (sum-js input))
+
+(printf "Day 12. Part One: ~s~n" part-one)
+(check-equal? part-one 156366)
