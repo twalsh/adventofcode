93e99edbd999bdd330ee0bbdf24c6d7bab7b3050
Tom Walsh <walshtp@gmail.com>
2016-01-14 08:37:57 +0000

    debug

--- advent14.rkt
+++ advent14.rkt
@@ -47,12 +47,25 @@
 (printf "~s~n" paths)
 (map length paths)
 
-(for/fold ((score1 0) (score2 0))
-  ((f1 (car paths)) (f2 (cadr paths)))
-  (printf "COMP C ~s ~s : D ~s ~s~n" f1 score1 f2 score2)
-  (cond ((> f1 f2)
-         (values (add1 score1) score2))
-        ((< f1 f2)
-         (values score1 (add1 score2)))
-        (else
-         (values (add1 score1) (add1 score2)))))
+(define score1 0)
+(define score2 0)
+
+(define p1 (car paths))
+(define p2 (cadr paths))
+
+p1
+p2
+
+(for ((i (in-range (length p1)))) 
+  (let ((f1 (list-ref p1 i)) (f2 (list-ref p2 i)))
+    (printf "COMP ~s C ~s ~s : D ~s ~s~n" i f1 score1 f2 score2)
+    (cond ((> f1 f2)
+           (set! score1 (add1 score1)))
+          ((< f1 f2)
+           (set! score2 (add1 score2)))
+          (else
+           (set! score1 (add1 score1))
+           (set! score2 (add1 score2))))))
+
+score1
+score2
