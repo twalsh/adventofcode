a6a4f6de37346b579a28f32a20709414b4df6c0f
Tom Walsh <walshtp@gmail.com>
2016-12-01 12:50:49 +0000

    Day 1a completed

--- day1.rkt
+++ day1.rkt
@@ -1,37 +1,41 @@
 #lang racket
 
 (require rackunit)
+(require "advent-utils.rkt")
 
 (define test-directions (map (lambda (s) (string-split s  ", "))
                              '("R2, L3"
                                "R2, R2, R2"
                                "R5, L5, R5, R3")))
 
-(define (go moves [n-s 0] [e-w 0] [direction 'north])
+(define (follow-directions moves [n-s 0] [e-w 0] [direction 'north])
   (if (empty? moves)
       (+ (abs n-s) (abs e-w))
       (let ((next-move (first moves)))
         (define distance (string->number (substring next-move 1)))
         (if (eq? (string-ref next-move 0) #\R)
             (cond ((eq? direction 'north)
-                   (go (rest moves) n-s (+ e-w distance) 'east))
+                   (follow-directions (rest moves) n-s (+ e-w distance) 'east))
                   ((eq? direction 'east)
-                   (go (rest moves) (+ n-s distance) e-w 'south))
+                   (follow-directions (rest moves) (+ n-s distance) e-w 'south))
                   ((eq? direction 'south)
-                   (go (rest moves) n-s (- e-w distance) 'west))
+                   (follow-directions (rest moves) n-s (- e-w distance) 'west))
                   ((eq? direction 'west)
-                   (go (rest moves) (- n-s distance) e-w 'north)))
+                   (follow-directions (rest moves) (- n-s distance) e-w 'north)))
             (cond ((eq? direction 'north)
-                   (go (rest moves) n-s (- e-w distance) 'west))
+                   (follow-directions (rest moves) n-s (- e-w distance) 'west))
                   ((eq? direction 'west)
-                   (go (rest moves) (+ n-s distance) e-w 'south))
+                   (follow-directions (rest moves) (+ n-s distance) e-w 'south))
                   ((eq? direction 'south)
-                   (go (rest moves) n-s (+ e-w distance) 'east))
+                   (follow-directions (rest moves) n-s (+ e-w distance) 'east))
                   ((eq? direction 'east)
-                   (go (rest moves) (- n-s distance) e-w 'north)))))))
+                   (follow-directions (rest moves) (- n-s distance) e-w 'north)))))))
 
-(check-equal? (go (first test-directions)) 5 "Test example 1")
-(check-equal? (go (second test-directions)) 2 "Test example 2")
-(check-equal? (go (third test-directions)) 12 "Test example 3")
+(check-equal? (follow-directions (first test-directions)) 5 "Test example 1")
+(check-equal? (follow-directions (second test-directions)) 2 "Test example 2")
+(check-equal? (follow-directions (third test-directions)) 12 "Test example 3")
 
+(define puzzle-data (string-split (car (read-input "input1.dat")) ", "))
+puzzle-data
 
+(follow-directions puzzle-data)
