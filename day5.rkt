129bdbb987f89026709a5b729159ea79b5800bab
Tom Walsh <walshtp@gmail.com>
2016-12-05 11:41:57 +0000

    Refactor

--- day5.rkt
+++ day5.rkt
@@ -5,25 +5,39 @@
 
 (define test-door-id "abc")
 
-(define (next-hash door-id [start 0])
-  (define md5-output (md5 (string-append door-id (number->string start))))
-  (define md5-prefix (take (bytes->list md5-output) 5))
-  (if (equal? md5-prefix '(48 48 48 48 48))
-      (values md5-output start (integer->char (bytes-ref md5-output 5)))
-      (next-hash door-id (add1 start))))
+(define (valid-hash? md5-hash)
+  #t)
 
-(define (door-code door-id)
-  (define door-code-letters
-    (let loop ([index 0] [letter-count 0])
-      (cond ((= letter-count 8) '())
-            (else 
-             (define-values (output last-index letter) (next-hash door-id index))
-             (cons letter (loop (add1 last-index) (add1 letter-count)))))))
-  (list->string door-code-letters))
-  
-;(check-true (string-prefix? (door-code test-door-id) "18f"))
+(define (make-hash-finder valid-hash?)
+  (define (hash-finder door-id start)
+    (define md5-output (md5 (string-append door-id (number->string start))))
+    (define md5-prefix (take (bytes->list md5-output) 5))
+    (if (and (equal? md5-prefix '(48 48 48 48 48))
+             (valid-hash? md5-output))
+        (values md5-output start (integer->char (bytes-ref md5-output 5)))
+        (hash-finder door-id (add1 start))))
+  hash-finder)
 
-(define puzzle-input "reyedfim")
-(define puzzle-answer (door-code puzzle-input))
+(define (make-decoder hash-validator)
+  (define hash-finder (make-hash-finder hash-validator))
+  (define (decoder door-id)
+    (define door-code-letters
+      (let loop ([index 0] [letter-count 0])
+        (cond ((= letter-count 8) '())
+              (else 
+               (define-values (output last-index letter) (hash-finder door-id index))
+               (cons letter (loop (add1 last-index) (add1 letter-count)))))))
+    (list->string door-code-letters))
+  decoder)
 
-(define puzzle-solution "f97c354d")
+(define simple-decoder (make-decoder valid-hash?))
+
+(check-true (string-prefix? (simple-decoder test-door-id) "18f"))
+
+;(define puzzle-input "reyedfim")
+;(define puzzle-answer (door-code puzzle-input))
+
+;(define puzzle-solution "f97c354d")
+
+(define complex-hash-validator? 
+
