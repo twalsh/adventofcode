a3fa3b8882b35861124a59ecbc71d78ca5dac1e3
Tom Walsh <walshtp@gmail.com>
2016-12-06 09:29:42 +0000

    Replace (transform) with (rows->columns)

--- day3.rkt
+++ day3.rkt
@@ -22,11 +22,7 @@
         '()
         (cons (take lines 3) (loop (drop lines 3))))))
 
-(define (transform rows)
-  (for/list ((i (in-range 3)))
-    (map (lambda (r) (list-ref r i)) rows)))
-
-(define transformed-triangles (map transform vertical-triangles))
+(define transformed-triangles (map rows->columns vertical-triangles))
 
 (for/sum ((triplet transformed-triangles))
   (count valid-triangle? triplet))
