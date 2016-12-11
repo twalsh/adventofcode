8e3027266dfd9dc27f4d8d0ddc03d42a4e46cbba
Tom <walshtp@gmail.com>
2016-12-11 19:00:21 +0000

    checkpoint

--- day11.rkt
+++ day11.rkt
@@ -3,15 +3,13 @@
 (require racket/random)
 (require racket/set)
 
-(define floors
+(define (start-floors)
   (for/vector ((items
                 '((HM LM)
                   (HG)
                   (LG)
                   ())))
     (list->mutable-set items)))
-
-(displayln floors)
 
 (define complete-set '(HG HM LG LM))
 
@@ -27,28 +25,46 @@
                 (not (set-member? floor 'HG)))
            (and (set-member? floor 'LM)
                 (not (set-member? floor 'LG))))))
-                        
-(let loop ([floors floors]
-           [floor-number 0]
-           [current-path '()]
-           [paths '()]
-           [steps 0])
-  (displayln floors)
-  (cond
-    ((> steps 1) 'stop)
-    ((floor-complete? (vector-ref floors 3)) floors)
-    (else
-     (define floor (vector-ref floors floor-number))
-     ; Discard empty pick list
-     (define picks (rest (combinations (set->list floor))))
-     (displayln picks)
-    
-     (for/list ((direction directions))
-       (for/list ((pick picks))
-         (define new-floors (set-copy floors))
-         (define this-floor (vector-ref new-floors floor-number))
-         (define next-floor (vector-ref new-floors (+ floor-number direction)))
-         (set-add next-floor pick)
-         (set-remove! this-floor pick)
-         new-floors)))))
-         
+
+(define (all-floors-ok? floors)
+  (for/and ((i (in-range 1 4)))
+    (floor-ok? (vector-ref floors i))))
+
+(define (print-floors floors)
+  
+  (for ((i (in-range 4)))
+    (displayln (set->list (vector-ref floors i)))))
+
+(define test         
+  (let loop ([floors (start-floors)]
+             [floor-number 0]
+             [steps 0])
+    (cond
+      ((floor-complete? (vector-ref floors 3)) (cons floors steps))
+      ((not (all-floors-ok? floors))
+       (displayln floors)
+       'stop)
+      ((> steps 10) floors)
+      (else
+       
+       (define floor (vector-ref floors floor-number))
+       (define picks (rest (combinations (set->list floor))))
+       (define sub-trees
+         (for/list ((direction directions)
+                    ; Cannot move down from floor 0 or up from floor 3
+                    #:when (not (or (and (= floor-number 0) (= direction -1))
+                                    (and (= floor-number 3) (= direction 1)))))
+           (for/list ((pick picks))
+             ; Move picks between floors
+             (for ((p pick))
+                (define new-floors
+                  (for/vector ((i (in-range 4)))
+                    (set-copy (vector-ref floors i))))
+               (define this-floor (vector-ref new-floors floor-number))
+               (define next-floor (vector-ref new-floors (+ floor-number direction)))
+               (set-add! next-floor p)
+               (set-remove! this-floor p)
+               (loop new-floors (+ floor-number direction) (add1 steps))))))
+       (cons (cons floors steps) sub-trees)))))
+             
+              
