2c4ccd04215f2dc12b7bd07634a38cd4a27f7b4e
Tom Walsh <walshtp@gmail.com>
2016-12-12 10:20:00 +0000

    Use 'any flag with read-line

--- advent-utils.rkt
+++ advent-utils.rkt
@@ -7,7 +7,7 @@
   (read-lines (open-input-file file)))
 
 (define (read-lines in [ lines '()])
-  (let ((line (read-line in)))
+  (let ((line (read-line in 'any)))
     (if (eof-object? line)
         (reverse lines)
         (read-lines in (cons line lines)))))
