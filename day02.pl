0b1a18e95d1febdd95a15622ff0c961757925640
Tom Walsh <walshtp@gmail.com>
2017-12-02 20:08:54 +0000

    Day 02

--- day02.pl
+++ day02.pl
@@ -4,10 +4,30 @@ use File::Slurp;
 use List::Util qw/min max sum/;
 
 my @rows = read_file('day02.in'); 
+my @cells = map { [ split q{ }, $_ ] } @rows;
 
-my @diffs = map {
-    my @cells = split q{ }, $_;
-    max(@cells) - min(@cells);
-} @rows;
+sub checksum {
+    my $func = shift;
+    my @row_values = map { $func->($_) } @cells;
+    sum(@row_values);
+}
 
-say sum(@diffs);
+say checksum(sub { max(@{$_}) - min(@{$_}) }); 
+
+my $find_divisors = sub {
+    my @row = @{$_};
+    for my $i (0 .. $#row-1){
+        my $m = $row[$i];
+        for my $j ($i+1 .. $#row){
+            my $n = $row[$j];
+            if ($m % $n == 0){
+                return $m / $n;
+            }
+            elsif ($n % $m == 0){
+                return $n / $m;
+            }
+        }
+    }
+};
+
+say checksum($find_divisors); 
