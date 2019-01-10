bb3b47f7535cc4c1593f4ec6043d3f8e22a31340
Tom Walsh <walshtp@gmail.com>
2019-01-10 22:55:53 +0000

    Refactor

--- advent4.rkt
+++ advent4.rkt
@@ -4,12 +4,25 @@
 
 (define key "bgvyzdsv")
 
-(let loop ((i 0))
-  ;(display i)(newline)
-  (let ((string (string-join  (list key (number->string i)) "")))
-    (let ((hash (md5 (open-input-string string))))
-      (if (string=? (substring hash 0 5) "00000")
-          (cons i hash)
-          (loop (+ i 1))))))
-       
+(define (number->md5 i)
+    (md5 (open-input-string
+             (string-append key 
+                            (number->string i)))))
+
+(define (zero-prefix n [start 0])
+  (define prefix (make-string n #\0))
   
+  (let loop ((i start))
+    (let ((hash (number->md5 i))) 
+      (if (string=? (substring hash 0 n) prefix)
+        (values i hash)
+        (loop (+ i 1))))))
+           
+(define-values (part-one-n part-one-hash)
+               (zero-prefix 5))
+(printf "Part One: ~a~n" part-one-n)
+
+(define-values (part-two-n part-two-hash)
+               (zero-prefix 6 part-one-n))
+(printf "Part Two ~a~n" part-two-n)
+
