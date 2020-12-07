5dc4e2645441bfa4d4fb5a3b933eb3a3ae761e63
Tom Walsh <walshtp@gmail.com>
2020-12-07 14:24:52 +0000

    Day 7

--- 20/day7.py
+++ 20/day7.py
@@ -2,16 +2,6 @@
 
 import re
 
-test_input="""light red bags contain 1 bright white bag, 2 muted yellow bags.
-dark orange bags contain 3 bright white bags, 4 muted yellow bags.
-bright white bags contain 1 shiny gold bag.
-muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
-shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
-dark olive bags contain 3 faded blue bags, 4 dotted black bags.
-vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
-faded blue bags contain no other bags.
-dotted black bags contain no other bags.""".split('\n')
-
 
 def make_containers(data):
     contains = {}
@@ -23,20 +13,18 @@ def make_containers(data):
         m = rule.findall(line)
     #    print(m)
         container = m[0][1]
-        contains[container] = []
+        contains[container] = {} 
         for content in m[1:]:
             number,colour = content
-                
-            contains[container].append({ colour: number }) 
+            if number:
+                contains[container][colour] = int(number)
+            
             if not colour in contained_by:
                 contained_by[colour] = { container }
             else:
                 contained_by[colour].add(container)
     return(contained_by,contains)  
 
-test_bag = 'shiny gold'
-
-(test_contained_by,_) = make_containers(test_input)
 
 def find_containers(colour,contained_by):
     containers = {} 
@@ -49,11 +37,44 @@ def find_containers(colour,contained_by):
     else:
         return {} 
 
+def find_contents(primary_colour,contains):
+    print('COLOUR',primary_colour)
+
+    count = 0 
+    if primary_colour in contains:
+        for colour,num in contains[primary_colour].items():
+            count = count + num 
+            contents_count = find_contents(colour,contains)
+            for i in range(num):
+                count = count + contents_count
+    return count
+
+test_input="""light red bags contain 1 bright white bag, 2 muted yellow bags.
+dark orange bags contain 3 bright white bags, 4 muted yellow bags.
+bright white bags contain 1 shiny gold bag.
+muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
+shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
+dark olive bags contain 3 faded blue bags, 4 dotted black bags.
+vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
+faded blue bags contain no other bags.
+dotted black bags contain no other bags.""".split('\n')
+
+test_bag = 'shiny gold'
+
+(test_contained_by,test_contents) = make_containers(test_input)
+
 assert len(find_containers(test_bag,test_contained_by)) == 4
 
 f = open('input7.txt')
 
 lines = [ line[:-1] for line in f ]
 
-(contained_by,_) = make_containers(lines)
+(contained_by,puzzle_contents) = make_containers(lines)
 print('Part One:',len(find_containers(test_bag,contained_by)))
+
+test_count = find_contents(test_bag,test_contents)
+print(test_count)
+assert test_count == 32
+
+puzzle_count = find_contents(test_bag,puzzle_contents)
+print('Part Two',puzzle_count)
